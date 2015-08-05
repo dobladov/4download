7e9ae57349a219d783343e7c17bfa7969fc5b35c
DobladoV <danieldoblado@gmail.com>
2015-08-05 19:39:29 +0200

    Add total download size, check if file exist, network error check

--- 4download.py
+++ 4download.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 import sys
+import os.path
 import urllib.request
 from pyquery import PyQuery as pq
 
+totalDownload = 0
+
 def downloadFile(url,fileName):
+    global totalDownload
     print("Downloading: " + fileName)
     file = urllib.request.urlretrieve(url, fileName)
+    totalDownload += int(file[1]["Content-Length"])
     print("Downloaded: " + fileName + " " + file[1]["Content-Length"] + " bytes")
 
 def filterLinks(urls, filter):
@@ -15,6 +20,7 @@ def filterLinks(urls, filter):
         if url.split("/")[-1].split(".")[-1] == filter:
             result.append(url)
     return result
+
 
 try: # URL Argument
     url = sys.argv[1]
@@ -27,7 +33,11 @@ except IndexError:
     filter = ""
 
 ## Get URLs
-d = pq(url, headers={'user-agent': 'pyquery'})
+try:
+    d = pq(url, headers={'user-agent': 'pyquery'})
+except UnboundLocalError:
+    exit("Network error")
+
 elements = d(".fileText a")
 links = []
 for element in elements:
@@ -45,5 +55,9 @@ print("Found " + str(total) + " resources.")
 # Download links
 for i, link in enumerate(links, start=1):
     print("File "+ str(i) + " of " + str(total) + " - " + str(round(((i / total) * 100.0),2)) + "%")
-    downloadFile(link["url"].replace("//", "http://"), link["name"] );
-exit("All files Downloaded!")
+    if not os.path.exists(link["name"]):
+        downloadFile(link["url"].replace("//", "http://"), link["name"] )
+    else:
+        print("Already downloaded")
+
+exit("All files Downloaded! " + str(round((totalDownload / 1024 / 1024),2)) + "Mb")
